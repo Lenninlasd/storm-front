e5264c6de4edfc29dd9d81c1e02c5c8a9d85a315
lasdjs <lenninlasd@gmail.com>
2015-08-29 12:48:22 -0500

    keyboard controller

--- app/components/keyboard/keyboard.js
+++ app/components/keyboard/keyboard.js
@@ -10,7 +10,20 @@ angular
       controller: keyboardCtrl,
       templateUrl: 'components/keyboard/keyboard.html'
     };
-    function keyboardCtrl($scope, $element, $attrs) {
-      // body...
-    }
+  }
+
+  function keyboardCtrl($scope, $element, $attrs) {
+    $scope.keyboard = "";
+    $scope.keyboardNumber = "";
+
+    $scope.addNumber = function (number) {
+        $scope.keyboardNumber = $scope.keyboardNumber + number;
+    };
+    $scope.setNumber = function () {
+        $scope.keyboard = $scope.keyboardNumber;
+    };
+    $scope.clearKeyboard = function () {
+        $scope.keyboard = "";
+        $scope.keyboardNumber = "";
+    };
   }
