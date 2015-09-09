c2ca4e984640e6bf18380755a690f5efe5f2f1ef
lasdjs <lenninlasd@gmail.com>
2015-09-09 00:14:15 -0500

    responsive keyboard y mejoras en el token gen

--- app/components/tokenScreen/tokenScreen.js
+++ app/components/tokenScreen/tokenScreen.js
@@ -9,9 +9,23 @@ angular
       retrict: 'E',
       scope: {},
       controller: tokenScreenCtrl,
-      templateUrl: 'components/tokenScreen/tokenScreen.html'
+      templateUrl: 'components/tokenScreen/tokenScreen.html',
+      link: tokenScreenLink
     };
   }
 
   function tokenScreenCtrl($scope, $element, $attrs) {
+  }
+
+  function tokenScreenLink(scope, element, attrs) {
+      var w = angular.element(window);
+      scope.fontSize = "1.6em";
+      if (screen.width <= 600) {
+          scope.fontSize = "1em";
+      }
+
+      w.bind('resize',function(){
+          scope.fontSize = screen.width <= 600 ? "1em" : "1.6em";
+          scope.$apply();
+      });
   }
