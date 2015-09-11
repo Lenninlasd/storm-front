70354219a33620648266bdab12093dc295ced6e2
lasdjs <lenninlasd@gmail.com>
2015-09-11 08:04:29 -0500

    configuracion de gulp para generacion css

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,21 @@ var gulp = require('gulp');
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require("gulp-rename");
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('default', ['sass', 'sass:watch']);
 
 gulp.task('sass', function () {
     'use strict';
-    console.log('lennin');
     gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(concat('flugel.css'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename('flugel.min.css'))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('sass:watch', function () {
