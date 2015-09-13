2caf45a59eba0cd3f3d0fdd9f7cc8b5314942aee
lasdjs <lenninlasd@gmail.com>
2015-09-13 00:30:11 -0500

    maquetacion tokenManagement

--- app/src/components/tokenManagement/tokenManagement.js
+++ app/src/components/tokenManagement/tokenManagement.js
@@ -7,14 +7,26 @@ angular
   function fgTokenManagementDirective() {
     return {
       retrict: 'E',
-      scope: {},
+      scope: {
+      },
       controller: tokenManagementCtrl,
+      controllerAs: 'demo',
       templateUrl: 'src/components/tokenManagement/tokenManagement.html',
       link: tokenManagementLink
     };
   }
 
   function tokenManagementCtrl($scope, $element, $attrs) {
+    var self = this;
+    self.hidden = false;
+    self.direction = 'up';
+    self.onlyRead = $attrs.onlyRead === 'true' ? true : false;
+    self.items = [
+        {name: "Nueva transacción", icon: "fa-plus", direction: "left" },
+        {name: "Reclasificar servicio", icon: "fa-reply", direction: "left" },
+        {name: "Tranferir turno", icon: "fa-exchange", direction: "left" },
+        {name: "Terminar turno", icon: "fa-power-off", direction: "left", btnColor: "md-warn"}
+    ];
   }
 
   function tokenManagementLink(scope, element, attrs) {
