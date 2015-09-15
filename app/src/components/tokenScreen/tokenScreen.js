683a4b7bdff0d71b731a90f720b5612c22b484ba
lasdjs <lenninlasd@gmail.com>
2015-09-14 23:42:24 -0500

    socketio en token gen

--- app/src/components/tokenScreen/tokenScreen.js
+++ app/src/components/tokenScreen/tokenScreen.js
@@ -14,7 +14,20 @@ angular
     };
   }
 
-  function tokenScreenCtrl($scope, $element, $attrs) {
+  function tokenScreenCtrl($scope, $element, $attrs, Token) {
+      $scope.pendingTokens = [];
+      var socket = io('http://localhost:5000');
+
+      Token.tokens.query({state: 0}, function (data) {
+          $scope.pendingTokens = data;
+      });
+
+      socket.on('newToken', function (data) {
+          console.log(data);
+          Token.tokens.query({state: 0}, function (data) {
+              $scope.pendingTokens = data;
+          });
+      });
   }
 
   function tokenScreenLink(scope, element, attrs) {
--- serve.js
+++ serve.js
@@ -1,12 +1,17 @@
 var express = require('express'),
  	app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io')(server),
  	port = 8000;
 
-var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-http.listen(port, function(){
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+server.listen(port, function(){
 	console.log('Listen on Port ' + port);
 });
