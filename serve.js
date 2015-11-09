af6ae356ccefcd50d9922843bf60e566a6532b6f
zenerlasd <lenninlasd@gmail.com>
2015-11-09 16:06:47 -0500

    inicio de session

--- serve.js
+++ serve.js
@@ -8,6 +8,7 @@ var express = require('express'),
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/login', express.static(__dirname + '/login'));
 app.use('/', express.static(__dirname + '/landing'));
+app.use('/img', express.static(__dirname + '/img'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 io.on('connection', function(socket){
