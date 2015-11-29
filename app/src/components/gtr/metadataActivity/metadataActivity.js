9399cdbf2489e09381559808ceaabcf537f32010
Lennin <lenninlasd@gmail.com>
2015-11-29 18:12:12 -0500

    metadata actividad cliente y asesor

--- app/src/components/gtr/metadataActivity/metadataActivity.js
+++ app/src/components/gtr/metadataActivity/metadataActivity.js
@@ -16,18 +16,77 @@ angular
     };
   }
 
-  metadataActivityCtrl.$inject = ['$scope', '$element','$attrs'];
-  function metadataActivityCtrl($scope, $element, $attrs) {
+  metadataActivityCtrl.$inject = ['$scope', '$element','$attrs', '$interval'];
+  function metadataActivityCtrl($scope, $element, $attrs, $interval) {
       // $scope.$apply();
-      var customer;
-      var adviser;
+      var customers;
+      var advisers;
+
+      $scope.roles = [];
+      $scope.activities = [];
+      $scope.customersOnHold = [];
+      $scope.customersAttended = [];
+
       $scope.$watch('allData', function () {
           if (!$scope.allData.adviser || !$scope.allData.customer) return;
 
-          customer = $scope.allData.customer;
-          adviser = $scope.allData.adviser;
-          console.log(adviser);
+          customers = $scope.allData.customer;
+          advisers = $scope.allData.adviser;
+          $scope.roles = getRoles(advisers);
+          $scope.activities = getActivity(advisers);
+
+          $scope.customersOnHold = getStateOnHold(customers);
+          $scope.customersAttended = getStateAttended(customers);
+
+          $interval(function () {
+              $scope.customersOnHold = getStateOnHold(customers);
+          }, 1000, false);
+
       });
+
+      function getRoles(advisers) {
+          return _.chain(advisers)
+          .map(function (adviser) {return adviser.activity.role;})
+          .groupBy(function (rol) {return rol.name;})
+          .mapObject(function (val, key) {return {'rol': key, 'code': val[0].code, 'count' : val.length};})
+          .values()
+          .value();
+      }
+
+      function getActivity(advisers) {
+          return _.chain(advisers)
+          .map(function (adviser) {return adviser.activity.activityEvent;})
+          .groupBy(function (activity) {return activity.eventName;})
+          .mapObject(function (val, key) {return {'eventName': key, 'eventCode': val[0].eventCode, 'count' : val.length};})
+          .values()
+          .value();
+      }
+
+      function getStateAttended(customers) {
+          return _.chain(customers)
+          .filter(function (customer) {return customer.token.receiverAdviser;})
+          .map(function (customer) {
+              var momentIniTime = moment(customer.token.infoToken.logCreationToken);
+              return {time: moment.duration(moment().diff(momentIniTime)).asMinutes()};
+          })
+          .groupBy(function (timeObj) {return timeObj.time <= 15 ? 'puntual' : 'impuntual';})
+          .mapObject(function (val, key) {return {'state' : key, 'count' : val.length};})
+          .values()
+          .value();
+      }
+
+      function getStateOnHold(customers) {
+          return _.chain(customers)
+          .filter(function (customer) {return !customer.token.receiverAdviser;})
+          .map(function (customer) {
+              var momentIniTime = moment(customer.token.infoToken.logCreationToken);
+              return {time: moment.duration(moment().diff(momentIniTime)).asMinutes()};
+          })
+          .groupBy(function (timeObj) {return timeObj.time <= 15 ? 'puntual' : 'impuntual';})
+          .mapObject(function (val, key) {return {'state' : key, 'count' : val.length};})
+          .values()
+          .value();
+      }
 
   }
 })();
